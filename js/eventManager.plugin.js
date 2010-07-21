27eb823aa27f828ba267000f95daea6ca9a5128d
Olivier <og@agencecosmic.com>
2010-07-21 09:42:17 +0200

    Added a debug option

--- js/eventManager.plugin.js
+++ js/eventManager.plugin.js
@@ -1,6 +1,13 @@
 ;(function($) {
   // Event Manager lets you organize your javascript events by using the power of the delegate method
-  $.fn.eventManager = function(events) {
+  $.fn.eventManager = function(events,options) {
+    var opts = $.extend({}, { debug:false }, options);
+     
+    // private function for debugging
+    function debug($obj) {
+      if (window.console && window.console.log) { window.console.log($obj); }
+    }
+    
     var eventList = {}, // yeah this is the event list array
         $global = $(this); // the object container of the event list
     
@@ -22,12 +29,22 @@
           if ($(this).is(globalSelector_p)) {
             var selectors = events[globalSelector_p][e.type];
             if (typeof selectors == 'function') {
+              if (opts.debug) { 
+                 debug("    selector : $('"+globalSelector_p+"')\n"
+                         + "event called : "+e.type+"\n"
+                         + "      action : "+ events[globalSelector_p][e.type]);
+              }
               if (selectors.apply(this, [e, $global]) === false) {
                  return false;
               }
             } else {
               for (selector_p in selectors) {
                 if (($(this).is(selector_p) || selector_p == 'default') && typeof selectors[selector_p] != 'undefined') {
+                  if (opts.debug) { 
+                      debug("    selector : $('"+globalSelector_p+"').filter('"+selector_p+"')\n"
+                              + "called event : "+e.type+"\n"
+                              + "      action : "+ events[globalSelector_p][e.type][selector_p]);
+                  }
                   if (selectors[selector_p].apply(this, [e, $global]) === false) {
                      return false;
                   }
